7d058bb2225c5b8bcfbe5dfcf2c2598c830d2c24
Huang Lulu <2195625465@qq.com>
2026-05-05 23:45:20 +0800

    P1

--- P1/nlp4j_baseline/run_hmm_grid.cmd
+++ P1/nlp4j_baseline/run_hmm_grid.cmd
@@ -1,9 +1,9 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-set TEST_PATH=..\datasets\auto\test.txt
-set MODEL_PATH=model\hmm_bmes_model.json
-set OUTPUT_ROOT=output_grid
+set "TEST_PATH=..\datasets\auto\test.txt"
+set "MODEL_PATH=model\hmm_bmes_model.json"
+set "OUTPUT_ROOT=output_grid"
 
 if not exist "%TEST_PATH%" (
   echo Missing test: %TEST_PATH%
@@ -26,16 +26,33 @@ for %%I in (-0.2 -0.5 -1.0) do (
     set TAG=!TAG:-=m!
     set TAG=!TAG:.=p!
 
-    set OUTDIR=%OUTPUT_ROOT%\!TAG!
+    set "OUTDIR=!OUTPUT_ROOT!\!TAG!"
     if not exist "!OUTDIR!" mkdir "!OUTDIR!"
+    if not exist "!OUTDIR!\" (
+      echo Failed to create output dir: !OUTDIR!
+      exit /b 1
+    )
 
     echo Running itp=%%I sp=%%S ep=%%S
     mvn -q exec:java -Dexec.mainClass=Nlp4jHmmPredictor -Dexec.args="%TEST_PATH% %MODEL_PATH% output %%I %%S %%S"
     if errorlevel 1 exit /b 1
 
-    copy /Y output\nlp4j_hmm_metrics.json "!OUTDIR!\"
-    copy /Y output\nlp4j_hmm_label_report.tsv "!OUTDIR!\"
-    copy /Y output\nlp4j_hmm_confusion_matrix.tsv "!OUTDIR!\"
+    if not exist "output\nlp4j_hmm_metrics.json" (
+      echo Missing output\nlp4j_hmm_metrics.json
+      exit /b 1
+    )
+    if not exist "output\nlp4j_hmm_label_report.tsv" (
+      echo Missing output\nlp4j_hmm_label_report.tsv
+      exit /b 1
+    )
+    if not exist "output\nlp4j_hmm_confusion_matrix.tsv" (
+      echo Missing output\nlp4j_hmm_confusion_matrix.tsv
+      exit /b 1
+    )
+
+    copy /Y output\nlp4j_hmm_metrics.json "!OUTDIR!\" >nul
+    copy /Y output\nlp4j_hmm_label_report.tsv "!OUTDIR!\" >nul
+    copy /Y output\nlp4j_hmm_confusion_matrix.tsv "!OUTDIR!\" >nul
   )
 )
 
